68f62b1d17e499a505cd2627be7a13ec1c636896
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-02-18 23:38:11 +0100

    at least basic rendering works

--- src/app.js
+++ src/app.js
@@ -1,5 +1,77 @@
-import { creatures } from "../creature-data.js";
-
+const creatures = [
+  {
+    species: "Triceratops",
+    weight: 13000,
+    height: 114,
+    diet: "herbavor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "First discovered in 1889 by Othniel Charles Marsh",
+  },
+  {
+    species: "Tyrannosaurus Rex",
+    weight: 11905,
+    height: 144,
+    diet: "carnivor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "The largest known skull measures in at 5 feet long.",
+  },
+  {
+    species: "Anklyosaurus",
+    weight: 10500,
+    height: 55,
+    diet: "herbavor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Anklyosaurus survived for approximately 135 million years.",
+  },
+  {
+    species: "Brachiosaurus",
+    weight: 70000,
+    height: "372",
+    diet: "herbavor",
+    where: "North America",
+    when: "Late Jurasic",
+    fact: "An asteroid was named 9954 Brachiosaurus in 1991.",
+  },
+  {
+    species: "Stegosaurus",
+    weight: 11600,
+    height: 79,
+    diet: "herbavor",
+    where: "North America, Europe, Asia",
+    when: "Late Jurasic to Early Cretaceous",
+    fact: "The Stegosaurus had between 17 and 22 seperate places and flat spines.",
+  },
+  {
+    species: "Elasmosaurus",
+    weight: 16000,
+    height: 59,
+    diet: "carnivor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Elasmosaurus was a marine reptile first discovered in Kansas.",
+  },
+  {
+    species: "Pteranodon",
+    weight: 44,
+    height: 20,
+    diet: "carnivor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Actually a flying reptile, the Pteranodon is not a dinosaur.",
+  },
+  {
+    species: "Pigeon",
+    weight: 0.5,
+    height: 9,
+    diet: "herbavor",
+    where: "World Wide",
+    when: "Holocene",
+    fact: "All birds are living dinosaurs.",
+  },
+];
 class Creature {
   constructor(species, weight, height, diet, where, when, fact) {
     this.species = species;
@@ -100,7 +172,7 @@ function renderGrid(human) {
       const headlineContent = dino.species;
       headline.innerHTML = headlineContent;
 
-      const dinoUrl = `../images/${headlineContent.toLowerCase()}.png`;
+      const dinoUrl = `images/${headlineContent.toLowerCase()}.png`;
       image.setAttribute("src", dinoUrl);
 
       const paragraphContent = dino.fact;
@@ -109,7 +181,7 @@ function renderGrid(human) {
       const headlineContent = dino.name;
       headline.innerHTML = headlineContent;
 
-      const humanUrl = `../images/human.png`;
+      const humanUrl = `images/human.png`;
       image.setAttribute("src", humanUrl);
     }
   }
